292615ce412dea2e800593e4ba85acd423cb2bd8
Prajwal Ghatol <prajwalghatol00@gmail.com>
2023-12-19 19:09:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ImgtoPdf from './components/ImgtoPdf';
+
+function App() {
+  return (
+    <div>
+      <ImgtoPdf />
+    </div>
+  );
+}
+
+export default App;
